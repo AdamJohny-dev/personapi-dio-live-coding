225c60eea0ee8d832ec16c9288e63114462a6fdc
Adam John <adamjohny@get.inatel.br>
2020-12-28 20:39:44 -0300

    Developed unit tests for create person

---
+++ src/test/java/one/digitalinnovation/personapi/utils/PersonUtils.java
@@ -0,0 +1,36 @@
+package one.digitalinnovation.personapi.utils;
+
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
+import one.digitalinnovation.personapi.entity.Person;
+
+import java.time.LocalDate;
+import java.util.Collections;
+
+public class PersonUtils {
+
+    private static final String FIRST_NAME = "Adam";
+    private static final String LAST_NAME = "Johny da Mota";
+    private static final String CPF_NUMBER = "113.410.116-31";
+    private static final long PERSON_ID = 1L;
+    public static final LocalDate BIRTH_DATE = LocalDate.of(2010, 10, 1);
+
+    public static PersonDTO createFakeDTO() {
+        return PersonDTO.builder()
+                .firstName(FIRST_NAME)
+                .lastName(LAST_NAME)
+                .cpf(CPF_NUMBER)
+                .birthDate("04-04-2018")
+                .phones(Collections.singletonList(PhoneUtils.createFakeDTO()))
+                .build();
+    }
+
+    public static Person createFakeEntity() {
+        return Person.builder()
+                .firstName(FIRST_NAME)
+                .lastName(LAST_NAME)
+                .cpf(CPF_NUMBER)
+                .birthDate(BIRTH_DATE)
+                .phones(Collections.singletonList(PhoneUtils.createFakeEntity()))
+                .build();
+    }
+}
